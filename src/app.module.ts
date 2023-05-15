0dc4b1cc007cce78fbfc00345994d9aa21fedfee
0xdbe <23268141+0xdbe@users.noreply.github.com>
2023-05-15 15:06:55 +0200

    feat: bootstrap application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+const fs = require('fs');
+const path = require('path');
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const rootCertificate = path.join(__dirname, "certificates/us-east-2-bundle.pem");
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT, 10) || 5432,
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      entities: [],
+      synchronize: true,
+      ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(rootCertificate).toString()
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
